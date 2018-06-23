03cc6b50a8f0a74ab62c23868602112dca191f12
Arthur Lekane <arthur.lekane@orange.com>
2018-06-23 17:13:10 +0100

    trash

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>The best choice</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -80,6 +80,9 @@
             <div class="content">
                 <div class="title m-b-md">
                     Bienvenue sur the best choice
+                </div>
+                <div class="m-b-md">
+                    Disponible bientôt
                 </div>
             </div>
         </div>
